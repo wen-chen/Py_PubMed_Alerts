4bfb884e36517b2f6ae6778a70671c5a037234d9
Chen Wen <chenwen@biochen.com>
2017-03-18 17:10:17 +0800

    fix bug

--- PubMed_Alerts.py
+++ PubMed_Alerts.py
@@ -1,24 +1,40 @@
-from util.search_pubmed  import search, fetch_details
+from util.search_pubmed  import get_papers_list
 from util.send_email import send_mail
+import pickle
 
+task_list = list()
+with open("PubMed_Alerts.conf", "r") as conf:
+    for line in conf:
+        line = line.strip()
+        print(line)
+        task_id, keyword, mail_address = line.split()
+        task_list.append([task_id, keyword, mail_address]) 
 
-results = search('lncRNA')
-id_list = results['IdList']
-papers = fetch_details(id_list)
-content = str()
-for i, paper in enumerate(papers['PubmedArticle']):        
-    Title = paper['MedlineCitation']['Article']['ArticleTitle']
-    Abstract = paper['MedlineCitation']['Article']['Abstract']["AbstractText"]
-    PMID = paper['MedlineCitation']["PMID"].split(',')[0]
-    url = "https://www.ncbi.nlm.nih.gov/pubmed/" + PMID
-    content += str(i + 1) + ". " + Title + '\n'
-    content += str(Abstract) + '\n'
-    content += url + "\n\n"
-    
+def run(task):
+    task_id, keyword, mail_address = task
+    papers_list = get_papers_list(keyword)    
+    papers_set = set([item[0] for item in papers_list])
+    task_dump = open(task_id, 'rb')
+    papers_set = pickle.load(task_dump)
+    task_dump.close()
+    kown_papers = list()
+    for paper in papers_list:
+        if paper[0] in papers_set:
+            kown_papers.append(paper)
+    for paper in kown_papers:
+        papers_list.remove(paper)
+    if len(papers_list) > 0:
+        content = str()
+        for i, paper in enumerate(papers_list):
+            PMID, Title, Journal, IF, Month, Year, Abstract = paper
+            url = "https://www.ncbi.nlm.nih.gov/pubmed/" + PMID
+            content += str(i + 1) + ". " + Title + '\n'
+            content += Journal + "  " + "IF: " + IF + "  " + Month + '/' + Year + '\n'
+            content += "Abstract: " + str(Abstract) + '\n'
+            content += url + "\n\n"
+        mail_to_list = [mail_address]
+        subject = "PubMed Alerts: " + keyword
+        send_mail(mail_to_list, subject, content)     
 
-mail_to_list = ["chenwen@biochen.com"]          
-subject = "PubMed papers"
-if send_mail(mail_to_list, subject, content):  
-    print("Automatically send mail successfully")
-else:
-    print("Automatically send mail failed")  
+for task in task_list:
+    run(task) 
