26d6b7e8b6a77fc28870de18686d05941566d791
Chen Wen <chenwen@biochen.com>
2017-03-18 17:10:55 +0800

    fix bug

--- util/search_pubmed.py
+++ util/search_pubmed.py
@@ -1,12 +1,11 @@
-from Bio import Entrez
+from Bio import Entrez   
 
-def search(query, sort='relevance', retmax='20',retmode='xml', db='pubmed'):
+def search(query, retmax='20', retmode='xml', db='pubmed'):
     Entrez.email = 'admin@biochen.com'
-    handle = Entrez.esearch(db='pubmed', 
-                            sort='relevance', 
-                            retmax='20',
-                            retmode='xml', 
-                            term=query)
+    handle = Entrez.esearch(db = db, 
+                            retmax = retmax,
+                            retmode = retmode, 
+                            term = query)
     results = Entrez.read(handle)
     return results
 
@@ -18,6 +17,50 @@ def fetch_details(id_list):
                            id=ids)
     results = Entrez.read(handle)
     return results
+
+Journal_IF = dict()
+ISSN_IF = dict()
+with open("IF2016.tsv", "r") as IF2016:
+    for line in IF2016:
+        line = line.strip()
+        item = line.split('\t')
+        Journal_IF[item[0]] = item[3] 
+        ISSN_IF[item[2]] = item[3]
+
+def get_papers_list(keyword, retmax = "20"):
+    papers_list = list()
+    results = search(keyword, retmax = retmax)
+    id_list = results['IdList']
+    papers = fetch_details(id_list)
+    for i, paper in enumerate(papers['PubmedArticle']):        
+        Title = paper['MedlineCitation']['Article']['ArticleTitle']
+        try:
+            Abstract = paper['MedlineCitation']['Article']['Abstract']["AbstractText"]
+        except:
+            Abstract = "NA"
+        PMID = paper['MedlineCitation']["PMID"].split(',')[0]
+        try:
+            Year = paper['MedlineCitation']['Article']["Journal"]["JournalIssue"]["PubDate"]['Year']
+        except:
+            Year = "NA"
+        try:
+            Month = paper['MedlineCitation']['Article']["Journal"]["JournalIssue"]["PubDate"]['Month']
+        except:
+            Month = "NA"
+        Journal = paper['MedlineCitation']['Article']["Journal"]["Title"]
+        try:
+            ISSN = str(paper['MedlineCitation']['Article']["Journal"]["ISSN"])
+        except:
+            ISSN = "NA"
+        if Journal.upper() in Journal_IF:
+            IF = Journal_IF[Journal.upper()]
+        else:
+            if ISSN in ISSN_IF:
+                IF = ISSN_IF[ISSN]
+            else:
+                IF = "NA"                       
+        papers_list.append([PMID, Title, Journal, IF, Month, Year, Abstract])
+    return papers_list
 
 if __name__ == '__main__':
     results = search('lncRNA')
